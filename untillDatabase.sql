fd265053cd56d68d785b86fbec9622cca8d4dd1b
Oleg Kravchuck <ok@techno-les.com>
2021-12-10 01:16:06 +0200

    added keywords and added sorting index

--- untillDatabase.sql
+++ untillDatabase.sql
@@ -1,9 +1,7 @@
 drop schema read CASCADE;
-drop schema write CASCADE;
 drop schema public CASCADE;
 
 create schema read;
-create schema write;
 create schema public;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
